670dc10846a6b100ec6ce55ac8bfd7952b6b3360
Arbab Khan <ak.arbabkhan4@gmail.com>
2021-06-02 00:14:59 +0530

    Intigrating frontend and backend in process

---
+++ src/components/AfterLogin/DashboardComponent.js
@@ -0,0 +1,97 @@
+import React, {Component} from 'react';
+import { Navbar,NavbarBrand, Nav, NavbarToggler, Collapse, NavItem, Card, CardImgOverlay, CardTitle} from 'reactstrap';
+import {NavLink,Link} from 'react-router-dom';
+import { Loading } from './LoadingComponent';
+
+
+class Dashboard extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            isNavOpen: false
+        }
+        this.toggleNav = this.toggleNav.bind(this);
+    }
+
+    toggleNav(){
+        this.setState({
+            isNavOpen: !this.state.isNavOpen
+        });
+    }
+
+    
+    render(){
+
+      /*const lratings = this.props.ratings.filter((rating) => rating.wid === this.props.rating.id);
+
+      var sum = 0;
+      for( var i = 0; i < lratings.length; i++ ){
+          sum += parseInt( lratings[i].content, 10 ); //don't forget to add the base
+      }
+
+      var avg = (sum/lratings.length).toFixed(1);*/
+
+      if (this.props.workersLoading || this.props.clientsLoading){
+        return(
+            <div className="container">
+                <div className="row">
+                    <Loading />
+                </div>
+            </div>
+        )
+      }
+
+      else if (this.props.workersErrMess || this.props.clientsErrMess){
+        return(
+            <div className="container">
+                <div className="row">
+                    <h4>{this.props.workersErrMess}</h4>
+                </div>
+            </div>
+        )
+    }
+
+    else if(!this.props.workersLoading || this.props.clientsLoading){
+
+      const lworkers = this.props.workers.filter((worker) => this.props.client.address.area === worker.area)
+      const list = lworkers.map((worker) => {return (
+            <Card>
+                  <Link to={`/worker/${this.props.client.id}/${worker.id}`} >
+                      {/*<CardImg width="100%" src={worker.image} alt={worker.name} />*/}
+                      <CardImgOverlay></CardImgOverlay>
+                          <CardTitle>{worker.naam}</CardTitle>
+                  </Link>
+             </Card>
+          );
+      });
+
+        return(
+        <div>
+            <Navbar dark color="primary" expand="md">
+                    <div className="container">
+                        <NavbarToggler onClick={this.toggleNav} />
+                        <NavbarBrand href="/dashboard" > AYS | At Your Service</NavbarBrand>
+                        <Collapse isOpen={this.state.isNavOpen} navbar>
+                            <Nav className="ml-auto" navbar>
+                                <NavItem>
+                                    <NavLink className="nav-link" to={`/clientprofile/${this.props.client.id}`}>
+                                        My Profile
+                                    </NavLink>
+                                </NavItem>
+                                <NavItem>
+                                    <Link className="nav-link" to="/home">
+                                        Logout
+                                    </Link>
+                                </NavItem>
+                            </Nav>
+                        </Collapse>
+                    </div>
+            </Navbar>
+          {list}
+        </div>
+        );
+    }
+    }
+}
+
+export default Dashboard;
