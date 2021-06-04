5ab725d6fae5f49af42b6adebe310c8c46317255
Arbab Khan <ak.arbabkhan4@gmail.com>
2021-06-04 13:14:01 +0530

    Client SignUp form Updated!

--- src/components/ClientSignupComponent.js
+++ src/components/ClientSignupComponent.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
-import { Row, Col, Label, Button } from 'reactstrap';
-import { Control, LocalForm, Errors } from 'react-redux-form';
+import { Control } from 'react-redux-form';
+import video from '../videos/videosignin.mp4';
+import { Container, FormWrap, Icon, FormContent, Form, Formside, Formgrid, IndexBg, VideoBg, FormH1, FormLabel, FormButton } from './WorkerSignupComponentElements';
+
 
 class Signup_client extends Component {
 
@@ -18,88 +20,94 @@ class Signup_client extends Component {
 
     render(){
         return(
-            <div>
-                <LocalForm onSubmit={(values) => this.handleSubmit(values)}>
-                            <Row className="form-group">
-                                <Label htmlFor="name" md={2}>name</Label>
-                                <Col md={10}>
-                                    <Control.text model=".name" id="name" name="name"
+            <>
+            <Container>
+              <FormWrap>
+                <Icon to='/'>At Your Service</Icon>
+                <FormContent>
+                  <Form onSubmit={(values) => this.handleSubmit(values)}>
+                    <FormH1>Sign up to our application</FormH1>
+                    <Formside>
+                    <FormLabel htmlFor="name">Name</FormLabel>
+                          <Control.text model=".name" id="name" name="name"
                                         placeholder="Name"
                                         className="form-control"
                                          />
-                                </Col>
-                            </Row>
-                            <Row className="form-group">
-                                <Label htmlFor="pwd" md={2}>Password</Label>
-                                <Col md={10}>
-                                    <Control.text model=".pwd" id="pwd" name="pwd"
+                        </Formside>
+                      <Formside>
+                      <FormLabel htmlFor="pwd">Password</FormLabel>
+                      <div>
+                        <Control.text model=".pwd" id="pwd" name="pwd"
                                         placeholder="Password"
                                         className="form-control"
                                          />
-                                </Col>
-                            </Row>
-                            <Row className="form-group">
-                                <Label htmlFor="phno" md={2}>Ph no.</Label>
-                                <Col md={10}>
-                                    <Control.text model=".phno" id="phno" name="phno"
-                                        placeholder="Ph no."
-                                        className="form-control"
-                                         />
-                                </Col>
-                            </Row>
-                            <Row className="form-group">
-                                <Label htmlFor="email" md={2}>Email</Label>
-                                <Col md={10}>
-                                    <Control.text model=".email" id="email" name="email"
+                        </div>
+                        </Formside>
+                        <Formside>   
+                    <FormLabel htmlFor="phno">Ph no.</FormLabel>
+                    <div>
+                  <Control.text model=".phno" id="phno" name="phno"
+                                              placeholder="Ph no."
+                                              className="form-control"
+                                               />
+                 </div>
+      
+                 </Formside>
+                  <Formside>
+                 <FormLabel htmlFor="email">Email</FormLabel>
+                    <div>
+                  <Control.text model=".email" id="email" name="email"
                                         placeholder="Email"
                                         className="form-control" />
-                                </Col>
-                            </Row>
-                            <Row className="form-group">
-                                <Label htmlFor="hno" md={2}>House No.</Label>
-                                <Col md={10}>
-                                    <Control.text model=".hno" id="hno" name="hno"
+                 </div>
+                 </Formside>
+      
+                 <Formside>
+                 <FormLabel htmlFor="Address">Address</FormLabel>
+                <Formgrid>
+                  
+
+                   <Control.text model=".hno" id="hno" name="hno"
                                         placeholder="House No."
                                         className="form-control"
                                          />
-                                </Col>
-                            </Row>
-                            <Row className="form-group">
-                                <Label htmlFor="street" md={2}>Street</Label>
-                                <Col md={10}>
-                                    <Control.text model=".street" id="street" name="street"
+
+
+                   <Control.text model=".street" id="street" name="street"
                                         placeholder="Street"
                                         className="form-control"
                                          />
-                                </Col>
-                            </Row>
-                            <Row className="form-group">
-                                <Label htmlFor="city" md={2}>City</Label>
-                                <Col md={10}>
-                                    <Control.text model=".city" id="city" name="city"
+                                      
+                  </Formgrid>
+                  <Formgrid>
+                   <Control.text model=".city" id="city" name="city"
                                         placeholder="City"
                                         className="form-control"
                                          />
-                                </Col>
-                            </Row>
-                            <Row className="form-group">
-                                <Label htmlFor="state" md={2}>State</Label>
-                                <Col md={10}>
-                                    <Control.text model=".state" id="state" name="state"
+                  
+                  
+                   <Control.text model=".state" id="state" name="state"
                                         placeholder="State"
                                         className="form-control"
                                          />
-                                </Col>
-                            </Row>
-                            <Row className="form-group">
-                                <Col md={{size:10, offset: 2}}>
-                                    <Button type="submit" color="primary">
-                                    Register
-                                    </Button>
-                                </Col>
-                            </Row>
-                        </LocalForm>
-            </div>
+                  </Formgrid>
+                  </Formside>
+      
+                  <Formgrid>
+              
+                    <FormButton to='' type='submit' primary='' dark='false' smooth={true} duration={500} spy={true} exact='true' offset={-80}>Submit </FormButton>
+                    
+                
+              </Formgrid>
+              
+              </Form>
+              </FormContent>
+              </FormWrap>
+              <IndexBg>
+              <VideoBg autoPlay loop muted src={video} type='video/mp4' />
+            </IndexBg>
+            </Container>
+          </>
         )
     }
 }
